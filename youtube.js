d1d95afdb344aea2433cf43309cd5dfbb54ca5db
DevMark4245 <52924127+DevMark4245@users.noreply.github.com>
2022-06-30 17:10:50 +0800

    Add files via upload

--- youtube.js
+++ youtube.js
@@ -1,9 +1,5 @@
- 
-const mp3ButtonText = "MP3";
-const mp4ButtonText = "MP4";
-function mp3() { window.open(`https://yt.clit.repl.co/download/index.html?ytid=${ location.href.split('=')[1] }&format=mp3`); }
-function mp4() { window.open(`https://loader.to/?link=${ location.href }&f=7&s=1&e=1&r=loader`); }
- 
+const DownloadButtonText = "Download";
+function Download() { window.open(`https://loader.to/?link=${ location.href }&f=7&s=1&e=1&r=loader`); }
  
 const cssText = `
     .download-button {
@@ -48,29 +44,19 @@ const cssText = `
             document.querySelectorAll("#analytics-button:not(.download-panel)").forEach(panel => {
                 panel.classList.add("download-panel");
 
-                //MP4 button
-                const buttonMP4 = document.createElement("div");   //Create button
-                buttonMP4.classList.add("download-button");
-                buttonMP4.addEventListener("click", mp4);
-                const textMP4 = document.createElement("span");    //text
-                textMP4.classList.add("download-button-text");
-                textMP4.innerHTML = mp4ButtonText;
-                buttonMP4.appendChild(textMP4);                    //append text to button
-
-                //MP3 button
-                const buttonMP3 = document.createElement("div");   //Create button
-                buttonMP3.classList.add("download-button");
-                buttonMP3.addEventListener("click", mp3);
-                const textMP3 = document.createElement("span");    //text
-                textMP3.classList.add("download-button-text");
-                textMP3.innerHTML = mp3ButtonText;
-                buttonMP3.appendChild(textMP3);                    //append text to button
+                //Download button
+                const buttonDownload = document.createElement("div");   //Create button
+                buttonDownload.classList.add("download-button");
+                buttonDownload.addEventListener("click", Download);
+                const textDownload = document.createElement("span");    //text
+                textDownload.classList.add("download-button-text");
+                textDownload.innerHTML = DownloadButtonText;
+                buttonDownload.appendChild(textDownload);               //append text to button
 
                 //outer container (to keep buttons side-by-side)
                 const container = document.createElement("div");
                 container.classList.add("download-button-container");
-                container.appendChild(buttonMP3);
-                container.appendChild(buttonMP4);
+                container.appendChild(buttonDownload);
                 panel.insertBefore(container, panel.firstElementChild);
             });
         }, 200);
